5d43474d7ed89996b1c4a194864987d709ac2ea1
Martin <martingarcia804@yahoo.com>
2023-01-27 17:38:43 -0600

    Completed Lab

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = (num1, num2) => num1 / num2;
+
+divide(2000, 100);
+
+const square = parameterOne => parameterOne * parameterOne
+
+const add = (ranNum1, ranNum2) => ranNum1 + ranNum2
